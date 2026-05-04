0fde0660a3130f6a7aee104993087d54be640ae5
nodeserverlol-collab <nodeserverlol@gmail.com>
2026-05-04 16:36:49 +0300

    Initial commit: проект добавлен в репозиторий

--- frontend/vite-project/src/pages/api.tsx
+++ frontend/vite-project/src/pages/api.tsx
@@ -24,7 +24,7 @@ export interface RegisterData {
 }
 
 const api = axios.create({
-  baseURL: 'https://foodbackend-6ksq.onrender.com/api',
+  baseURL: 'https://localhost:3000',
   headers: {
     'Content-Type': 'application/json',
   },
